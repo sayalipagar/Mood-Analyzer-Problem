34e87272f9fcd5733ae4b21964eccd0d9bd1b939
sayalipagar <pagarsayali565@gmail.com>
2022-09-29 20:02:49 +0530

    [Sayali] ADD:UsingConstructorSadMoodMessage

--- MoodAnalyserTest/AnaylserTest.cs
+++ MoodAnalyserTest/AnaylserTest.cs
@@ -6,20 +6,12 @@ namespace MoodAnalyserTest
     public class AnaylserTest
     {
         [TestMethod]
-        public void GivenIAmInSadMood_ShouldReturnSad()
+        public void GivenIAmInSadMoodUsingConstructor_ShouldReturnSad()
         {
             string message = "I am in Sad Mood";
             MoodAnalyser moodAnalyser = new MoodAnalyser();
             string actual = moodAnalyser.Analyser(message);
             Assert.AreEqual(actual, "SAD");
-        }
-        [TestMethod]
-        public void GivenIAmInAnyMood_ShouldReturnHappy()
-        {
-            string message = "I am in Any Mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            string actual = moodAnalyser.Analyser(message);
-            Assert.AreEqual(actual, "HAPPY");
         }
     }
 }
