544f21a55e74a8be3544fd1439eba149a00ac86f
sayalipagar <pagarsayali565@gmail.com>
2022-09-29 18:48:41 +0530

    [Sayali] ADD:ReturnSadMessage

---
+++ MoodAnalyserTest/AnaylserTest.cs
@@ -0,0 +1,17 @@
+using MoodAnalyserProblem;
+
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class AnaylserTest
+    {
+        [TestMethod]
+        public void GivenIAmInSadMood_ShouldReturnSad()
+        {
+            string message = "I am in Sad Mood";
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string actual = moodAnalyser.Analyser(message);
+            Assert.AreEqual(actual, "SAD");
+        }
+    }
+}
