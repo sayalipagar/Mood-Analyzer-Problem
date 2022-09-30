826d48f9a67c6a5ddaddb6da842f9e46c33df36f
sayalipagar <pagarsayali565@gmail.com>
2022-09-30 15:52:36 +0530

    [sayali]Add:Empty-Mood-Throw-Exception

--- MoodAnalyserTest/AnaylserTest.cs
+++ MoodAnalyserTest/AnaylserTest.cs
@@ -3,46 +3,60 @@ using MoodAnalyserProblem;
 namespace MoodAnalyserTest
 {
     [TestClass]
-    public class MoodAnalyserTest
-    {
-        [TestMethod]
-        public void GivenIAmInSadMessageInConstructor_ShouldReturnSad()
+        public class MoodAnalyserTest
         {
-            string message = "I am in Sad Mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
-            string actual = moodAnalyser.Analyser();
-            Assert.AreEqual(actual, "SAD");
-        }
         [TestMethod]
-        public void GivenIAmInHappyMessageInConstructor_ShouldReturnHappy()
-        {
-            string message = "I am in Happy Mood";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
-            string actual = moodAnalyser.Analyser();
-            Assert.AreEqual(actual, "HAPPY");
-        }
-        [TestMethod]
-        public void GivenNullMessage_ShouldReturnHappy()
-        {
-            string message = "null";
-            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
-            string actual = moodAnalyser.Analyser();
-            Assert.AreEqual(actual, "HAPPY");
-        }
-        [TestMethod]
-        public void GivenNullMessage_WhenAnalyde_ShouldReturnHappy()
-        {
-            string message = "null";
-            MoodAnalyser mood = new MoodAnalyser(message);
-            try
+            public void GivenIAmInSadMessageInConstructor_ShouldReturnSad()
             {
-                string actual = mood.Analyser();
+                string message = "I am in Sad Mood";
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                string actual = moodAnalyser.Analyser();
+                Assert.AreEqual(actual, "SAD");
             }
-            catch (MoodAnalysisException ex)
+            [TestMethod]
+            public void GivenIAmInHappyMessageInConstructor_ShouldReturnHappy()
             {
-                Assert.AreEqual(ex.Message, "Message is Null");
+                string message = "I am in Happy Mood";
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                string actual = moodAnalyser.Analyser();
+                Assert.AreEqual(actual, "HAPPY");
+            }
+            [TestMethod]
+            public void GivenNullMessage_ShouldReturnHappy()
+            {
+                string message = "null";
+                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+                string actual = moodAnalyser.Analyser();
+                Assert.AreEqual(actual, "HAPPY");
+            }
+            [TestMethod]
+            public void GivenNullMessage_WhenAnalyde_ShouldReturnHappy()
+            {
+                string message = "null";
+                MoodAnalyser mood = new MoodAnalyser(message);
+                try
+                {
+                    string actual = mood.Analyser();
+                }
+                catch (MoodAnalysisException ex)
+                {
+                    Assert.AreEqual(ex.Message, "Message is Null");
+                }
+            }
+            [TestMethod]
+            public void GivenEmptylMessage_WhenAnalyde_ShouldReturnHappy()
+            {
+                string message = string.Empty;
+                MoodAnalyser mood = new MoodAnalyser(message);
+                try
+                {
+                    string actual = mood.Analyser();
+                }
+                catch (MoodAnalysisException ex)
+                {
+                    Assert.AreEqual(ex.Message, "Message is Empty");
+                }
             }
         }
 
     }
-}
